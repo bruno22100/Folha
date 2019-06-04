1f773c64665572439ef82290a38850ca08ab69cd
bruno22100 <brunorafaelsevero22@gmail.com>
2019-06-04 17:39:27 -0300

    refazendo a classe Empregado

--- src/projeto/Sistema.java
+++ src/projeto/Sistema.java
@@ -1,20 +1,6 @@
 package projeto;
-import java.util.Scanner;
 
 public class Sistema {
-	public Empregado adicionar()
-	{
-		String nome, endereco, tipo;
-		Scanner input = new Scanner(System.in);
-		System.out.print("Insina o nome do Empregado:\n");
-		nome = input.nextLine();
-		System.out.print("Insina o endereco do Empregado\n");
-		endereco = input.nextLine();
-		System.out.print("Insina o tipo do Empregado\n");
-		tipo = input.nextLine();
-		
-		Empregado empregado = new Empregado(nome, endereco, tipo);
-		return empregado;
-	}
+	
 
 }
